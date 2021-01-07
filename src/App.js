d3d0a420faf80c2b9c00f2978791f310e7f038b9
gauri38 <kumbhar.gauri@gmail.com>
2021-01-07 16:39:34 +0530

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Switch,Route} from 'react-router-dom';
+import StartPage from './component/StartPage';
+import MainPage from './component/MainPage';
+import Login from './component/Login';
+import SignUp from './component/SignUp';
+import Quiz from './component/Quiz';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={StartPage} />
+          <Route exact path="/home" component={MainPage} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/quiz" component={Quiz} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
--- src/component/Login.js
+++ src/component/Login.js
@@ -68,3 +68,6 @@ export default class Login extends react.Component {
     );
   }
 }
+
+
+
